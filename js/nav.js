63ec3b85f8adec2370d81fd78409912bc186a32f
Zara Leventhal <zleventhal@gmail.com>
2016-10-10 13:26:21 -0400

    inhibit scrolling mobile

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,20 @@
 $(function() {
     var toggleClass = $('.toggle-class');
     var navClass = $('.nav-class');
+    $("body").css("position", "relative"); // new
 
     $(toggleClass).click(function() {
-        $(navClass).toggleClass('show');
+        $(navClass).toggleClass('show'); // old
 
         var scroll = $("body").css("overflow");
+        var mobile = $("html").css("overflow");
+
         if (scroll == "hidden") {
             $("body").css("overflow", "visible");
+            $("html").css("overflow", "visible");
         } else {
             $("body").css("overflow", "hidden");
+            $("html").css("overflow", "hidden");
         }
 
     });
